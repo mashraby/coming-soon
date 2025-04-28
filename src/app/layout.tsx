d6464c8b14babbba032d1aa00d36c20ed57bc408
mashraby <yoldashovmashrab@gmail.com>
2025-04-28 13:00:54 +0500

    new version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,20 +10,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <title>Coming Soon</title>
-        <meta name="description" content="Something amazing is coming soon" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body className="font-sans antialiased">
         <ThemeProvider attribute="class" defaultTheme="dark">
           <Toaster position="top-center" />
